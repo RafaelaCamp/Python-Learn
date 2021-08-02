45f70afa3b5cc6c6852dcd0cfb9a03f785cda843
MeiBerry <campos.oli.rafaela@gmail.com>
2021-08-02 01:45:47 -0300

    Create medias.py

---
+++ PythonUSP/SemanaII/Exercício2/medias.py
@@ -0,0 +1,7 @@
+n1 = float(input("nota 1:"))
+n2 = float(input("nota 2:"))
+n3 = float(input("nota 3:"))
+n4 = float(input("nota 4:"))
+
+média = ((n1+n2+n3+n4)/4)
+print("A Média Aritmética é:", média)
