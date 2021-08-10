92b1fcad60a8756b653f54b0c233dc83046ef941
MeiBerry <campos.oli.rafaela@gmail.com>
2021-08-10 17:45:09 -0300

    Create Dezenas.py

---
+++ PythonUSP/SemanaII/Exercício5/Dezenas.py
@@ -0,0 +1,31 @@
+# Exercício 5 - Dígitos das Dezenas
+
+# Faça um programa em Python que recebe um número inteiro e imprime seu dígito das dezenas. Observe o exemplo abaixo:
+
+# Exemplo 1:
+
+#    Entrada de Dados:
+
+#Digite um número inteiro: 78615
+
+#    Saída de Dados:
+
+# O dígito das dezenas é 1
+
+# Exemplo 2:
+
+#    Entrada de Dados:
+
+# Digite um número inteiro: 2
+
+#    Saída de Dados:
+
+# O dígito das dezenas é 0
+
+# Dica: O operador "//" faz uma divisão inteira jogando fora o resto, ou seja, aquilo que é menor que o divisor. O operador "%" devolve apenas o resto da divisão inteira jogando fora o resultado, ou seja, tudo que é maior ou igual ao divisor.
+
+x = int(input("Digite um numero inteiro:"))
+
+d = (x // 10) % 10
+
+print("O digito das dezenas é", d)
