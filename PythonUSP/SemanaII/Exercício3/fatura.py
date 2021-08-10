93990038b4c9dc7325b6f061f435d447801b5517
MeiBerry <campos.oli.rafaela@gmail.com>
2021-08-10 17:09:09 -0300

    Create fatura.py

---
+++ PythonUSP/SemanaII/Exercício3/fatura.py
@@ -0,0 +1,7 @@
+n1 = input("Digite o nome do cliente:")
+n2 = input("Digite o dia de vencimento:")
+n3 = input("Digite o mês de vencimento:")
+n4 = input("Digite o valor da fatura:")
+
+print("Olá,", n1)
+print("A sua fatura com vencimento em", n2, "de", n3, "no valor de R$", n4, "está fechada.")
