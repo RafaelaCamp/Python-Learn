3ecf11ef82cb2f28e0217f22a38c7c3d9bf9fffc
MeiBerry <campos.oli.rafaela@gmail.com>
2021-08-10 17:33:28 -0300

    Update medias.py

--- PythonUSP/SemanaII/Exercício2/medias.py
+++ PythonUSP/SemanaII/Exercício2/medias.py
@@ -1,3 +1,23 @@
+# Exercício 2 - Médias
+
+# Faça um programa em Python que receba quatro notas, calcule e imprima a média aritmética. Observe o exemplo abaixo:
+
+# Exemplo:
+
+#    Entrada de Dados:
+
+# Digite a primeira nota: 4
+
+# Digite a segunda nota: 5
+
+# Digite a terceira nota: 6
+
+# Digite a quarta nota: 7
+
+#    Saída de Dados:
+
+# A média aritmética é 5.5
+
 n1 = float(input("nota 1:"))
 n2 = float(input("nota 2:"))
 n3 = float(input("nota 3:"))
