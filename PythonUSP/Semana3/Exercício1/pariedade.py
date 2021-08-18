4721986f497f8715de47b86d3663521766a248f0
MeiBerry <campos.oli.rafaela@gmail.com>
2021-08-18 01:03:18 -0300

    Create pariedade.py

---
+++ PythonUSP/Semana3/Exercício1/pariedade.py
@@ -0,0 +1,18 @@
+# Exercício 1 - Par ou ímpar?
+
+# Receba um número inteiro na entrada e imprima
+
+# par 
+
+# quando o número for par ou
+
+# ímpar
+
+# quando o número for ímpar.
+
+x = int(input("digite um número:"))
+# se x é divisível por 2 = par
+if (x%2) == 0:
+  print("Par")
+else:
+  print("Ímpar")
