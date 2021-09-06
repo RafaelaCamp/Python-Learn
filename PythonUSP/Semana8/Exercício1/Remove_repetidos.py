bc02eb7a4916ffaa2b10d8f8d84c2ba0ecf62c29
MeiBerry <campos.oli.rafaela@gmail.com>
2021-09-06 15:08:27 -0300

    Create Remove_repetidos.py

---
+++ PythonUSP/Semana8/Exercício1/Remove_repetidos.py
@@ -0,0 +1,22 @@
+# Exercício 1 - Removendo elementos repetidos
+
+# Escreva a função remove_repetidos que recebe como parâmetro uma lista com números inteiros, verifica se tal lista possui elementos repetidos e os remove. A função deve devolver uma lista correspondente à primeira lista, sem elementos repetidos. A lista devolvida deve estar ordenada.
+
+# Dica: Você pode usar lista.sort() ou sorted(lista). Qual a diferença?
+
+# Exemplo:
+# 1 >>>lista = [2, 4, 2, 2, 3, 3, 1]
+# 2 >>>remove_repetidos(lista)
+# 3 [1, 2, 3, 4]
+# 4 >>>remove_repetidos([1, 2, 3, 3, 3, 4])
+# 5 [1, 2, 3, 4]
+
+def remove_repetidos(lista): #def lista antes e colocar sort() apenas vai deixar a lista original ordenada, não a cópia.
+    norepeat = []
+    for i in lista:
+        if i not in norepeat:
+            norepeat.append(i)
+    norepeat.sort()
+    print(norepeat)
+
+remove_repetidos([2, 2 , 1, 3])
